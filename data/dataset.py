ac2bd62ceecc8a1110f0bc376c5cf51619e2b247
JLoarteS <jorge.loarte@alumnos.upm.es>
2025-04-10 18:57:38 +0200

    Modified Pokemon class, damage calculation not done at all

--- data/dataset.py
+++ data/dataset.py
@@ -1,17 +1,49 @@
 import json
 import pandas as pd
 
-def get_all_pokemons():
+from pokemon import Pokemon, Move
+
+def get_all_pokemons(random_moves : bool = False):
     # JSON with pokemons
     with open("pokemons.json", "r") as f:
-        pokemons = json.load(f)
+        pokemons_json = json.load(f)
+    
+    pokemons = []
+    
+    movements = get_all_movements()
+    
+    for id in pokemons_json:        
+        name = pokemons_json[id]["name"]
+        type1 = pokemons_json[id]["types"][0]
+        type2 = pokemons_json[id]["types"][1] if len(pokemons_json[id]["types"]) > 1 else "joker"
+        attack = pokemons_json[id]["attack"]
+        defense = pokemons_json[id]["defense"]
+        moves = pokemons_json[id]["moves"]
+        
+        movesList = []
+        for move in moves:
+            for m in movements:
+                if move == str(m):
+                    movesList.append(m)
+                
+        pokemons.append(Pokemon(id, name, type1, attack, defense, movesList, type2, random_moves))
     
     return pokemons
 
-def get_all_movements():
+def get_all_movements():    
     # JSON with movements
     with open("movements.json", "r") as f:
-        movements = json.load(f)
+        movements_json = json.load(f)
+    
+    movements = []
+    
+    for name in movements_json:
+        id = movements_json[name]["id"]
+        power = movements_json[name]["power"]
+        type = movements_json[name]["type"]
+        accuracy = movements_json[name]["accuracy"]
+        
+        movements.append(Move(name, id, power, type, accuracy))
     
     return movements
 
@@ -35,6 +67,6 @@ def get_types_matrix():
 
 # Test
 
-# get_all_pokemons()
-# get_all_movements()
-# get_types_matrix()
+# print(get_all_pokemons())
+# print(get_all_movements())
+# print(get_types_matrix())
--- data/pokemon.py
+++ data/pokemon.py
@@ -1,67 +1,92 @@
-import pokebase as pb
-import json
-
-from data.dataset import get_types_matrix
+import random
 
 class Move:
-    def __init__(self, name = None):
+    # name ; id ; power ; type ; accuaracy
+    def __init__(self, name : str, id : int, power : int, type : str, accuracy : int):
         self.name = name
-        
-        self.power = None
-        self.type = None
-        self.accuracy = None
+        self.id = id
+        self.power = power
+        self.type = type
+        self.accuracy = accuracy
+    
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.name == self.name and self.id == self.id
+        return False
 
-        if name is not None:
-            with open("movements.json", "r") as f:
-                movements = json.load(f)
-            self.power = movements[name]["power"]
-            self.type = movements[name]["type"]
-            self.accuracy = movements[name]["accuracy"]
+    def __str__(self) -> str:
+        return self.name
 
 class Pokemon:
-    # id ; nombre ; tipo1 ; tipo2 ; ataque ; defensa ; move1,2,3,4
-    def __init__(self, id):
+    # id ; name ; type1 ; type2 ; attack ; defense ; moveList ; move1,2,3,4
+    def __init__(self, id : int, name : str, type1 : str, attack : int, defense : int, moves : list, type2 : str = "joker", random_moves : bool = False):
         self.id = id
-        with open("pokemons.json", "r") as f:
-            poke = json.load(f)
-        self.name = poke[str(id)]["name"]
-        self.type1 = poke[str(id)]["types"][0]
-        if(len(poke[str(id)]["types"]) == 2):
-            self.type2 = poke[str(id)]["types"][1]
+        self.name = name
+        self.type1 = type1
+        self.type2 = type2
+        self.attack = attack
+        self.special_attack = attack
+        self.defense = defense
+        self.special_defense = defense
+        self.moves = moves
+        self.move1 = None
+        self.move2 = None
+        self.move3 = None
+        self.move4 = None
+        
+        if random_moves:
+            self.move1 = self.get_random_move()
+            self.move2 = self.get_random_move()
+            self.move3 = self.get_random_move()
+            self.move4 = self.get_random_move()
+    
+    def get_random_move(self) -> Move:
+        
+        # Auxiliar list of movements
+        aux = self.moves.copy()
+        if self.move1 in aux:
+            aux.remove(self.move1)
+        if self.move2 in aux:
+            aux.remove(self.move1)
+        if self.move3 in aux:
+            aux.remove(self.move1)
+        if self.move4 in aux:
+            aux.remove(self.move1)
+        
+        if len(aux) > 0:
+            las = random.choice(aux)
+            print(las, type(las))
+            return las
         else:
-            self.type2 = "joker"
-        self.attack = poke[str(id)]["attack"]
-        self.defense = poke[str(id)]["defense"]
-        self.movementList = poke[str(id)]["moves"]
-        self.move1 = Move()
-        self.move2 = Move()
-        self.move3 = Move()
-        self.move4 = Move()
-
-def damage_calculation(poke_attacker:Pokemon, poke_defender:Pokemon):
-    LEVEL = 100
-    CRITICAL = 1
-    RANDOM = 100
-
-    def fun_damage(attacker, move, defender):
-        LEVEL = 100
-        CRITICAL = 1
-        RANDOM = 100
-
-        if move.name == None:
-            return 0
-        
-        
-        base = (((2 * LEVEL * CRITICAL / 5 + 2) * move.power * poke_attacker.attack / poke_defender.defense) / 50)
-        return ((base + 2) * stab(poke_attacker, move) * get_types_matrix()[defender.type1][move.type] * get_types_matrix()[defender.type2][move.type] * RANDOM * (move.accuracy/100))
-
-    def stab(poke:Pokemon, move:Move):
+            return None
+    
+    def STAB(self, move) -> float:
         ret = 1
-        if((poke.type1 == move.type) | (poke.type2 == move.type)):
+        if((self.type1 == move.type) | (self.type2 == move.type)):
             ret = 1.5
         else:
             ret = 1
         return ret
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+def damage_calculation(poke_attacker : Pokemon, poke_defender : Pokemon):
+    def fun_damage(attacker : Pokemon, move : Move, defender : Pokemon):
+        from dataset import get_types_matrix
+        
+        LEVEL = 100
+        CRITICAL = 1
+        RANDOM = 100
+        
+        print(move)
+
+        if move is None:
+            return 0
+        
+        base = (((2 * LEVEL * CRITICAL / 5 + 2) * move.power * attacker.attack / defender.defense) / 50)
+        return ((base + 2) * attacker.STAB(move) * get_types_matrix()[defender.type1][move.type] * get_types_matrix()[defender.type2][move.type] * RANDOM * (move.accuracy / 100))
         
 
     damage1 = fun_damage(poke_attacker, poke_attacker.move1, poke_defender)
@@ -70,4 +95,3 @@ def damage_calculation(poke_attacker:Pokemon, poke_defender:Pokemon):
     damage4 = fun_damage(poke_attacker, poke_attacker.move4, poke_defender)
     
     return damage1 + damage2 + damage3 + damage4
-    
